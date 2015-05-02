7f75785d1f9f26e98baa8b05bdf67dfe6d7173de
gutschilla <gutsch@zwoelf.net>
2015-05-02 23:25:02 +0200

    CurlWrapper uses Header/Body distinction (yikes"). Extended this library with parts and ideas of https://github.com/Mashape/unirest-php/blob/master/src/Unirest/Request.php

--- lib/CurlWrapper.php
+++ lib/CurlWrapper.php
@@ -1,37 +1,29 @@
 <?php
 class CurlWrapper {
-    // All of this taken from http://php.net/manual/de/function.curl-exec.php
+    // based on comment on http://php.net/manual/de/function.curl-exec.php
+    // extended with parts and ideas of https://github.com/Mashape/unirest-php/blob/master/src/Unirest/Request.php
 
     /** 
     * Send a POST requst using cURL 
     * @param string $url to request 
     * @param array $post values to send 
     * @param array $options for cURL 
-    * @return string 
+    * @return array( code => $code, body => $body, error => $error, header => $header )
     */ 
-    function curl_post($url, array $post = NULL, array $options = array()) 
-    { 
+    function curl_post($url, array $post = NULL, array $options = array()) { 
         $defaults = array( 
             CURLOPT_POST           => 1, 
-            CURLOPT_HEADER         => 0, 
+            CURLOPT_HEADER         => true, 
             CURLOPT_URL            => $url, 
             CURLOPT_FRESH_CONNECT  => 1, 
             CURLOPT_RETURNTRANSFER => 1, 
             CURLOPT_FORBID_REUSE   => 1, 
             CURLOPT_TIMEOUT        => 10, 
             CURLOPT_POSTFIELDS     => http_build_query($post) 
-        ); 
-    
-        $ch = curl_init(); 
-        curl_setopt_array($ch, ($options + $defaults)); 
-        if( ! $result = curl_exec($ch)) 
-        { 
-            trigger_error(curl_error($ch)); 
-        } 
-        curl_close($ch); 
-        return $result; 
-    } 
-    
+        );
+        return CurlWrapper::curl_process( $options, $defaults );    
+    }
+
     /** 
     * Send a GET requst using cURL 
     * @param string $url to request 
@@ -39,24 +31,42 @@ class CurlWrapper {
     * @param array $options for cURL 
     * @return string 
     */ 
-    function curl_get($url, array $get = NULL, array $options = array()) 
-    {    
+    function curl_get($url, array $get = NULL, array $options = array()) {    
         $defaults = array( 
             CURLOPT_URL => $url. (strpos($url, '?') === FALSE ? '?' : ''). http_build_query($get), 
-            CURLOPT_HEADER => 0, 
+            CURLOPT_HEADER => 1, 
             CURLOPT_RETURNTRANSFER => TRUE, 
             CURLOPT_TIMEOUT => 4 
-        ); 
+        );
         
-        $ch = curl_init(); 
-        curl_setopt_array($ch, ($options + $defaults)); 
-        if( ! $result = curl_exec($ch)) 
-        { 
-            trigger_error(curl_error($ch)); 
-        } 
-        curl_close($ch); 
-        return $result; 
+        return CurlWrapper::curl_process( $options, $defaults );    
+
     }     
     
+    function curl_process( $options, $defaults ){
+        
+        $handle = curl_init(); 
+        curl_setopt_array( $handle, ($options + $defaults)); 
+
+        $response = curl_exec( $handle);
+        $error    = curl_error( $handle);
+        $info     = curl_getinfo( $handle );
+
+        curl_close($handle);
+        
+        // Split the full response in its headers and body
+        $header_size = $info['header_size'];
+        $header      = substr($response, 0, $header_size);
+        $body        = substr($response, $header_size);
+        $status      = $info['http_code'];
+        
+        return array(
+            "header" => $header,
+            "body"   => $body,
+            "status" => $status,
+            "error"  => $error,
+        );
+    }
+        
 }
 ?>
