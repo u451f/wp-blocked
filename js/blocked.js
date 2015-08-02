4dffaee07f0ba4f052e0ef35f17478538dcc061e
u451f <u@451f.org>
2015-08-02 18:27:50 +0200

    wp suited ajax query to reload table results. still needs to run several times and only if there are no results.

--- js/blocked.js
+++ js/blocked.js
@@ -4,5 +4,27 @@ function checkURL(url) {
         string = "http://" + string;
     }
     url.value = string;
-    return url
+    return url;
 }
+
+jQuery(document).ready(function() {
+  //setInterval(function(){
+    jQuery.ajax({
+        beforeSend: function() {
+            jQuery("#table-results").append('<div id="blocked-results-loader />"');
+        },
+        url : myAjax.ajaxurl,
+        method: "POST",
+// fixme url et ssl
+        data: {action: "reload_blocked_results", url: "http://gmail.com", ssl: false },
+        dataType: "text",
+        cache: false,
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log("Error: " + xhr.status + thrownError);
+        },
+        success: function(response) {
+            jQuery('#blocked-results').html(response);
+        }
+    });
+  //}, 2000);
+});
